b9f1f0eecc53f622bf6342e65c2eb520fc282e98
Amina <amina.muradalieva91@gmail.com>
2023-12-10 20:31:43 +0600

    Added SaveInstanceState function and logging

--- app/src/main/java/com/example/onelab/MainActivity.kt
+++ app/src/main/java/com/example/onelab/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnStartIntent.setOnClickListener {
+        binding.btnNextActivity.setOnClickListener {
             startIntent()
 
         }
