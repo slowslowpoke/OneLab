5f807eb09de283c1c73c90e2f71fa90141f749ca
Amina <amina.muradalieva91@gmail.com>
2023-12-10 20:31:43 +0600

    Added SaveInstanceState function and logging

--- app/src/main/java/com/example/onelab/SecondaryActivity.kt
+++ app/src/main/java/com/example/onelab/SecondaryActivity.kt
@@ -1,11 +1,60 @@
 package com.example.onelab
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.onelab.databinding.ActivitySecondaryBinding
 
+
+private const val TAG = "Secondary Activity"
 class SecondaryActivity : AppCompatActivity() {
+
+    companion object {
+        private const val COUNTER = "counter"
+    }
+
+    private lateinit var binding: ActivitySecondaryBinding
+    private var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_secondary)
+        binding = ActivitySecondaryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d(TAG, "onCreate: $counter")
+
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt(COUNTER)
+            binding.tvConfChangesCounter.text = counter.toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(COUNTER, ++counter)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+
     }
 }
